2c09a3aefa920933969b17b95c6da9f49a905ef9
Priyom1 <soikatmirror23@gmail.com>
2024-07-12 04:22:38 +0600

    register work

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,17 +1,36 @@
 import React,{useState} from 'react'
 import Layout from '../../components/Layout/Layout'
-import {toast} from 'react-toastify'
+import toast from 'react-hot-toast';
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
   const[name,setName]=useState('');
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
   const [phone,setPhone] = useState('');
+  const navigate=useNavigate();
 
-  const handleSubmit=(e)=>{
+  const handleSubmit=async (e)=>{
     e.preventDefault();
-    console.log(name,email,password,phone);
-    toast.success('Register Succesfully');
+    try {
+      const res=await axios.post(
+        "/api/v1/auth/register",{name,email,password,phone}
+
+      );
+      if(res &&res.data.success){
+        toast.success(res.data.message);
+        navigate('/login');
+      }
+      else{
+        toast.error(res.data.message);
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error('Something went wrong')
+      
+    }
   };
 
 
@@ -22,7 +41,7 @@ const Register = () => {
             <h1>Register Now!</h1>
             <form onSubmit={handleSubmit}>
   <div className="mb-3">
-    <input type="text" value={name} onChange={(e)=> setName(e.target.value)}className="form-control" id="exampleInputEmail1" placeholder='Enter Your Name'required  />
+    <input type="text" value={name} onChange={(e)=> setName(e.target.value)}className="form-control" id="exampleInputName" placeholder='Enter Your Name'required  />
   </div>
   <div className="mb-3">
     
@@ -32,9 +51,9 @@ const Register = () => {
     <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder='Enter Password' required />
   </div>
   <div className="mb-3">
-    <input type="text" value={phone} onChange={(e)=> setPhone(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Number' required/>
+    <input type="text" value={phone} onChange={(e)=> setPhone(e.target.value)} className="form-control" id="exampleInputNumber" placeholder='Enter Your Number' required/>
   </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
+  <button type="submit"  className="btn btn-primary">Register</button>
 </form>
 
         </div>
