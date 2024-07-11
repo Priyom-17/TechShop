b1cec548dbc5fa6de0925f55adfb6991343df645
Priyom1 <soikatmirror23@gmail.com>
2024-07-11 13:58:54 +0600

    backend work

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
- import express from 'express'
-import colors from 'colors'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import connectDB from './config/db.js'
+import express from "express";
+import colors from "colors"; 
+import dotenv from 'dotenv';
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+
+
 dotenv.config();
 
 connectDB();
- const app = express();
 
- app.use(express.json());
- app.use(morgan("dev"));
+const app=express();
 
- app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>");
- });
+app.use(express.json())
+app.use(morgan('dev'))
 
- const PORT=process.env.PORT || 8080;
+app.use('/api/v1/auth',authRoutes);
 
- app.listen(PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`.bgCyan.white);
- });
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to the site</h1>')
+    })
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${PORT}`)
+});
