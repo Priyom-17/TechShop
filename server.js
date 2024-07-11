117d4fc9c133774cbd1ee4253720e6cadae2738b
Priyom1 <soikatmirror23@gmail.com>
2024-07-11 13:13:03 +0600

    initial work

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
- 
+ import express from 'express'
+import colors from 'colors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+ const app = express();
+
+ app.get("/",(req,res)=>{
+    res.send("<h1>Hello World</h1>");
+ });
+
+ const PORT=process.env.PORT || 8080;
+
+ app.listen(PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`.bgCyan.white);
+ });
